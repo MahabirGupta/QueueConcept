e44b0201ed642a3a1f0cb6c54efd2d8b3a38bf41
MahabirGupta <mahabirg81@gmail.com>
2024-08-08 22:23:45 +0800

    use element() or peek() method to get head element in the PriorityQueue collections
    use remove() poll() method to return and remove element

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.util.Iterator;
 import java.util.PriorityQueue;
+
+
 
 public class Main {
     public static void main(String[] args) {
@@ -11,8 +14,33 @@ public class Main {
         priorityQueue.add("B");
         priorityQueue.add("C");
         priorityQueue.offer("C"); //duplicates are allowed
+//        priorityQueue.offer(23); // not allowed in PriorityQueue. Only homogenous data is allowed not hetrogenuos
 
         System.out.println(priorityQueue); //insertion order preserved
+
+//        get head element using element() or peek() method
+//        System.out.println(priorityQueue.element()); //if empty it will return NoSuchElementException
+        System.out.println(priorityQueue.peek()); //if empty it will return null
+        System.out.println();
+
+// return and remove element from queue remove() poll()
+//        System.out.println(priorityQueue.remove()); //element A will be removed. If empty it will return NoSuchElementException
+//        System.out.println(priorityQueue);
+//        System.out.println(priorityQueue.poll()); //element B will be removed. If empty it will return null
+//        System.out.println(priorityQueue);
+
+//        To print out all the elements in the PriorityQueue using Iterator
+        Iterator iterator = priorityQueue.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        System.out.println();
+
+        //        To print out all the elements in the PriorityQueue using for.each loop
+        for (Object object:priorityQueue){
+            System.out.println(object);
+        }
 
     }
 }
