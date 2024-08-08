e48da9ed780b28dee52147047d5641b729583b3c
MahabirGupta <mahabirg81@gmail.com>
2024-08-08 20:49:15 +0800

    use add() and offer() method to add elements in the PriorityQueue collections

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        create PriortyQueue class
+        PriorityQueue priorityQueue = new PriorityQueue<>();
+
+//        add elements using add() or offer() method
+        priorityQueue.add("A");
+        priorityQueue.add("B");
+        priorityQueue.add("C");
+        priorityQueue.offer("C"); //duplicates are allowed
+
+        System.out.println(priorityQueue); //insertion order preserved
+
     }
 }
